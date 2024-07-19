aa2492bb3d9d59c1eabb4e7bb12c1a7a4d9a0d36
cjo2343 <cjo@information.dk>
2024-07-20 01:35:57 +0200

    Initial Commit

---
+++ SyncHealth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SyncHealth
+//
+//  Created by Christian Johnsen on 20/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SyncHealth/SyncHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncHealthApp.swift
+//  SyncHealth
+//
+//  Created by Christian Johnsen on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SyncHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
